84286b28bc7d05ac479e2254e45557467cf4a12f
DottoXD <80960175+DottoXD@users.noreply.github.com>
2021-12-15 20:12:04 +0100

    Create config.ts

---
+++ src/configuration/config.ts
@@ -0,0 +1,29 @@
+export type Configuration = {
+  botToken: string;
+  mongoUrl: string;
+  clientId: string;
+  botStatus: string;
+  updateInteractions: boolean;
+  mainGuild: string;
+  logsChannel: string;
+  mainColorCode: string;
+  mainChillColorCode: string;
+  botName: string;
+  apiUrl: string;
+  apiKey: string;
+};
+
+export const config: Configuration = {
+  botToken: '',
+  mongoUrl: '',
+  clientId: '',
+  botStatus: 'Prisme Winter, a  new Typescript bot.',
+  updateInteractions: true,
+  mainGuild: '',
+  logsChannel: '',
+  mainColorCode: '#93E7FB',
+  mainChillColorCode: '#F5EDD5',
+  botName: '☃️ Prisme Winter',
+  apiUrl: '',
+  apiKey: ''
+};
